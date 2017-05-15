0107bf6f2f72089d45ec8d5da333fd90311b995e
tongjinle <1328192478@qq.com>
2017-05-15 23:56:54 +0800

    定义到聊天的基本数据格式

--- server/logic/events.ts
+++ server/logic/events.ts
@@ -1,46 +1,69 @@
-const PLATFORM_EVENTS = {
+import * as SocketServer from 'socket.io';
+import * as _ from 'underscore';
+
+let PLATFORM_EVENTS = {
     // 平台操作
-    PLATFORM_USER_JOIN: "platform_user_join",
-    PLATFORM_USER_LEAVE: "platform_user_leave",
+    LOGIN: "",
+    LOGOUT: "",
 
     // 频道操作
-    CHANNEL_ADD: "channel_add",
-    CHANNEL_PAUSE: "channel_pause",
-    CHANNEL_REMOVE: "channel_remove",
-    CHANNEL_DISPOSE: "channel_dispose",
+    CHANNEL_ADD: "",
+    CHANNEL_PAUSE: "",
+    CHANNEL_REMOVE: "",
+    CHANNEL_DISPOSE: "",
 
-    CHANNEL_USER_JOIN: "channel_user_join",
-    CHANNEL_USER_LEAVE: "channel_user_leave",
+
 
     // 房间操作
-    ROOM_ADD: "room_add",
-    ROOM_PAUSE: "room_pause",
-    ROOM_REMOVE: "room_remove",
-    ROOM_DISPOSE: "room_dispose",
-
-    ROOM_USER_JOIN: "room_user_join",
-    ROOM_USER_LEAVE: "room_user_leave",
-    ROOM_USER_READY: "room_user_ready",
+    ROOM_ADD: "",
+    ROOM_PAUSE: "",
+    ROOM_REMOVE: "",
+    ROOM_DISPOSE: "",
 
 
-    // 
+
+
+    // 获取某个pathnode的所有用户
+    USERLIST: "",
 
     // 聊天
-    CHAT: "chat",
+    CHAT: "",
 
     // 游戏操作
-    GAME_ACT: "game_act"
+    GAME_ACT: "",
 
+    // 加入某个pathnode
+    ON_USER_JOIN: "",
+
+    // 离开某个pathnode
+    ON_USER_LEAVE: "",
+
+    // 接受到聊天信息
+    ON_CHAT: ""
 
 
 };
 
+// simple map
+for (let key in PLATFORM_EVENTS) {
+    let str: string = key.split('_')
+        .map((str, i) => {
+            if (i == 0) {
+                return str.toLowerCase();
+            } else {
+                return str[0].toUpperCase() + str.slice(1).toLowerCase();
+            }
+        })
+        .join('');
 
-import * as SocketServer from 'socket.io';
+    PLATFORM_EVENTS[key] = str;
+}
+
+
 // 事件的数据结构
 interface IPlatformfUserJoin {
-    username:string;
-    socket:SocketIO.Socket;
+    username: string;
+    socket: SocketIO.Socket;
 }
 
 

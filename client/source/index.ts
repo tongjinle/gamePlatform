62ea50d2faaa269c3714aab37fe9e2048528bdc2
tongjinle <1328192478@qq.com>
2017-05-21 00:18:38 +0800

    将socket.io.client添加到客户端

--- client/source/index.ts
+++ client/source/index.ts
@@ -1,9 +1,41 @@
 import * as _ from "underscore";
 import * as $ from "jquery";
+import * as io from "socket.io-client";
+import * as async from "async";
 
-
-_.forEach([1,2],n=>console.log(n));
-
-$(()=>{
-	$('body').html("hello world");
+let so: SocketIOClient.Socket = io("http://localhost:9527");
+async.parallel([
+	(cb) => {
+		$(() => {
+			$('body').html("hello world1!");
+			cb();
+		});
+	},
+	(cb) => {
+		so.on("connect", () => {
+			console.log(so.id);
+			cb();
+		});
+	}
+], (err, data) => {
+	!err && start();
 });
+
+function start() {
+
+	bindSocket();
+
+}
+
+_.forEach([1, 2], n => console.log(n));
+
+
+
+
+
+function bindSocket() {
+	so.emit("login", () => { })
+
+
+}
+
--- client/source/requireConfig.ts
+++ client/source/requireConfig.ts
@@ -3,6 +3,8 @@ require.config({
 	paths:{
 		"jquery":"../node_modules/jquery/dist/jquery",
 		"underscore":"../node_modules/underscore/underscore",
+        "socket.io-client":"../node_modules/socket.io-client/socket.io",
+        "async":"../node_modules/async/dist/async"
 	}
 });
 
